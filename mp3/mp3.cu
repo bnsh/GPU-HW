02d2b7b86f130954a5cd627abcd49abce284717e
Binesh Bannerjee <binesh_binesh@hotmail.com>
2014-09-15 14:30:01 -0400

    Auto commit from SOM - 2014-09-15 14:30:01

--- mp3/mp3.cu
+++ mp3/mp3.cu
@@ -11,48 +11,6 @@
 		}																	 \
 	} while(0)
 
-// Compute C = A * B
-__global__ void matrixMultiplyShared(const float * A, const float * B, float * C,
-						 int numARows, int numAColumns,
-						 int numBRows, int numBColumns,
-						 int numCRows, int numCColumns) {
-	//@@ Insert code to implement matrix multiplication here
-	//@@ You have to use shared memory for this MP
-	__shared__ float Atile[TILE_WIDTH][TILE_WIDTH];
-	__shared__ float Btile[TILE_WIDTH][TILE_WIDTH];
-
-	int Ar = blockIdx.y * blockDim.y + threadIdx.y;
-	int Bc = blockIdx.x * blockDim.x + threadIdx.x;
-
-	float Cvalue = 0.0;
-	int numtiles = (1+((numAColumns-1)/TILE_WIDTH));
-	for (int tile = 0; tile < numtiles; ++tile) {
-		int Ac = tile * TILE_WIDTH;
-		int Aidx = Ar * numAColumns + (Ac + threadIdx.x);
-		int Br = tile * TILE_WIDTH;
-		int Bidx = (Br+threadIdx.y) * numBColumns + Bc;
-
-		Atile[threadIdx.y][threadIdx.x] = A[Aidx];
-		Btile[threadIdx.y][threadIdx.x] = B[Bidx];
-		__syncthreads();
-		for (int i = 0; i < TILE_WIDTH; ++i) {
-			// Interesting. An array out of bounds _READ_ causes memory faults.
-			if (
-				(Ar < numARows) &&
-				((Ac + i) < numAColumns) &&
-				((Br + i) < numBRows) &&
-				(Bc < numBColumns)
-			) Cvalue += Atile[threadIdx.y][i] * Btile[i][threadIdx.x];
-		}
-		__syncthreads();
-	}
-
-	int Cr = Ar;
-	int Cc = Bc;
-	int Cidx = Cr * numCColumns + Cc;
-	if ((Cr < numCRows) && (Cc < numCColumns)) C[Cidx] = Cvalue;
-}
-
 static float *myImport(const char *fn, int *rows, int *cols) __attribute__((unused));
 static float *myImport(const char *fn, int *rows, int *cols) {
 	float *rv = NULL;
