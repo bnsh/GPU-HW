6fb825e87bac81393bc9fb649c039275d1789dba
Binesh Bannerjee <binesh_binesh@hotmail.com>
2014-09-15 03:15:01 -0400

    Auto commit from SOM - 2014-09-15 03:15:01

--- mp6/mp6.cu
+++ mp6/mp6.cu
@@ -1,14 +1,14 @@
 #include	<wb.h>
 
 
-#define wbCheck(stmt) do {													\
-		cudaError_t err = stmt;											   \
-		if (err != cudaSuccess) {											 \
-			wbLog(ERROR, "Failed to run stmt ", #stmt);					   \
-			wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));	\
-			return -1;														\
-		}																	 \
-	} while(0)
+#define wbCheck(stmt) do { \
+	cudaError_t err = stmt; \
+	if (err != cudaSuccess) { \
+		wbLog(ERROR, "Failed to run stmt ", #stmt); \
+		wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err)); \
+		return -1; \
+	} \
+} while(0)
 
 const int BLOCKSZ = 32;
 #define Mask_width  5
