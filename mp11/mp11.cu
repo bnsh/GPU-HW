27b31eccd3bf4d1bb5019464a56639e33b8d0363
Binesh Bannerjee <binesh_binesh@hotmail.com>
2015-03-10 17:00:02 -0400

    Auto commit from SOM - 2015-03-10 17:00:02

--- mp11/mp11.cu
+++ mp11/mp11.cu
@@ -16,6 +16,7 @@ int main(int argc, char ** argv) {
 	float * hostInputImageData;
 	float * hostOutputImageData;
 	const char * inputImageFile;
+	unsigned char *ucharImage = NULL;
 
 	//@@ Insert more code here
 
@@ -28,14 +29,22 @@ int main(int argc, char ** argv) {
 	imageWidth = wbImage_getWidth(inputImage);
 	imageHeight = wbImage_getHeight(inputImage);
 	imageChannels = wbImage_getChannels(inputImage);
+	hostInputImageData = (float *)(malloc(sizeof(float)*imageWidth*imageHeight*imageChannels));
+	hostOutputImageData = (float *)(malloc(sizeof(float)*imageWidth*imageHeight*imageChannels));
+	ucharImage = (unsigned char *)(malloc(sizeof(unsigned char)*imageWidth*imageHeight*imageChannels));
 	outputImage = wbImage_new(imageWidth, imageHeight, imageChannels);
 	wbTime_stop(Generic, "Importing data and creating memory on host");
+
+	castFromImageToUnsignedChar(
 
 	//@@ insert code here
 
 	wbSolution(args, outputImage);
 
 	//@@ insert code here
+	if (ucharImage != NULL) free(ucharImage); ucharImage = NULL;
+	if (hostOutputImageData != NULL) free(hostOutputImageData); hostOutputImageData = NULL;
+	if (hostInputImageData != NULL) free(hostInputImageData); hostInputImageData = NULL;
 
 	return 0;
 }
