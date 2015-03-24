fc8172814610a847434bae0de8dd0c6d2be955af
Binesh Bannerjee <binesh_binesh@hotmail.com>
2015-03-24 14:45:01 -0400

    Auto commit from SOM - 2015-03-24 14:45:01

--- mp12/mp12.cu
+++ mp12/mp12.cu
@@ -2,6 +2,8 @@
 
 __global__ void vecAdd(float * in1, float * in2, float * out, int len) {
 	//@@ Insert code to implement vector addition here
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < len) out[idx] = in1[idx] + in2[idx];
 }
 
 int main(int argc, char ** argv) {
@@ -20,11 +22,17 @@ int main(int argc, char ** argv) {
 	hostInput1 = (float *) wbImport(wbArg_getInputFile(args, 0), &inputLength);
 	hostInput2 = (float *) wbImport(wbArg_getInputFile(args, 1), &inputLength);
 	hostOutput = (float *) malloc(inputLength * sizeof(float));
+	cudaMalloc(&deviceInput1, inputLength * sizeof(float));
+	cudaMalloc(&deviceInput2, inputLength * sizeof(float));
+	cudaMalloc(&deviceOutput, inputLength * sizeof(float));
 	wbTime_stop(Generic, "Importing data and creating memory on host");
 
 
 	wbSolution(args, hostOutput, inputLength);
 
+	cudaFree(deviceOutput); deviceOutput = NULL;
+	cudaFree(deviceInput2); deviceInput2 = NULL;
+	cudaFree(deviceInput1); deviceInput1 = NULL;
 	free(hostInput1);
 	free(hostInput2);
 	free(hostOutput);
